780a84209954abe5b13c09be3f8bbfa39e8d9054
Jake8796 <jake8796@gmail.com>
2024-04-15 21:18:54 -0700

    modified:   MIDI2LED-Simplified.ino * Added comments and simplified code

--- MIDI2LED-Simplified.ino
+++ MIDI2LED-Simplified.ino
@@ -1,76 +1,73 @@
 #include <MIDI.h>
 #include <FastLED.h>
 
-
-// How many leds in your strip?
+// Number of LEDs in the strip
 #define NUM_OF_ROWS 64
+// Number of Rows in the grid
 #define SIZE_OF_ROW 8
 #define NUM_LEDS NUM_OF_ROWS*SIZE_OF_ROW
-#define LED 17
-#define DATA_PIN 2
+#define LED 17    //PIN of debug LED
+#define DATA_PIN 2  //Data pin of the individually addressable LED                                
 
 #define ON_BIT_MASK 0x80
+#define ON    1
+#define OFF   0
 
-
-
-// Globals
-static const BaseType_t pro_cpu = 0;
-static const BaseType_t app_cpu = 1;
+// Globals for thread processes
+static const BaseType_t pro_cpu = 0; // Assign pro_cpu to core 0
+static const BaseType_t app_cpu = 1; // Assign app_cpu to core 1
 static TaskHandle_t setFrameTask = NULL;
-//static portMUX_TYPE spinlock = portMUX_INITIALIZER_UNLOCKED;
-static QueueHandle_t msg_queue; 
-enum { MSG_QUEUE_LEN = 20 }; // Number of slots in message queue
-static SemaphoreHandle_t mutex;
+static QueueHandle_t midi_queue; 
+#define MIDI_QUEUE_LEN 20 // Number of slots in byte queue
+static SemaphoreHandle_t mutex; // Mutex for the byte queue
 
-//Delays
+//Delay in milliseconds
 static const uint32_t showFrameDelay = 100; 
 static const uint32_t setFrameDelay = 50; 
 
+//Create a MIDI object on the second serial bus
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, midi2);
 
+//Structure to hold LED coordinates
 typedef struct coord{
-  int x;
-  int y;
+  uint8_t x;
+  uint8_t y;
 }coord;
 
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
-int firstLedRow[NUM_OF_ROWS];
-int ledRowsQueue[NUM_OF_ROWS];
+uint8_t firstLedRow[NUM_OF_ROWS];
+uint8_t ledRowsQueue[NUM_OF_ROWS];
 
-
-int enqueueBit(int byteQueue, int thing) {
-  // Value must be 1 or zero
-  if (thing == 1) {
-    byteQueue = byteQueue << 1;
-    byteQueue = byteQueue | thing;
-  } else {
-    byteQueue = byteQueue << 1;
-  }
+// Use a byte as a queue to determine if LED is on or off
+uint8_t enqueueBit(uint8_t byteQueue, uint8_t bit) {
+  // Value must a boolean
+  byteQueue <<= 1;
+  byteQueue |= bit; //Enqueue bit
   return byteQueue;
 }
 
+// Show the rendered frame on the LED display
 void showFrame(void *parameters){
   
   while(1){
-
+    // Update the LEDs on the screen 
     FastLED.show();
     if (xSemaphoreTake(mutex, 0) == pdTRUE) {
       for(int row = 0; row < NUM_OF_ROWS; row++){
         //Enqueue the first led row into the queue
         ledRowsQueue[row] = enqueueBit(ledRowsQueue[row], firstLedRow[row]);
         int bitMask = 1;
-        for(int i = 0; i < SIZE_OF_ROW; i++){
-          //Set led to red if bit is 1 and set led to black if bit is 0
+        for(int col = 0; col < SIZE_OF_ROW; col++){
+          // Create a coord from row and column value
+          coord point{row, col};
+          int index = MatrixToLedString(point, SIZE_OF_ROW);
+          // Set led to red if bit is 1 and set led to black if bit is 0
           if((bitMask & ledRowsQueue[row]) == bitMask){
-            coord point{row, i};
             Serial.println(row);
-            int index = MatrixToLedString(point, SIZE_OF_ROW);
             leds[index] = CRGB::Red;
           }else{
-            coord point{row, i};
-            int index = MatrixToLedString(point, SIZE_OF_ROW);
             leds[index] = CRGB::Black;
           }
           bitMask = bitMask<<1;
@@ -79,25 +76,27 @@ void showFrame(void *parameters){
       }
         xSemaphoreGive(mutex);
     }
-      
+    // Delay the task
     vTaskDelay(showFrameDelay / portTICK_PERIOD_MS);
   }
    
 }
-
+// Render the frame 
 void setFrame(void *parameters){
   while(1){
-    int pitchToIndex;
-    coord point;
+    int pitchToIndex;//Take the pitch from MIDI and convert it to the index on the LED matrix
+    coord point; //XY coords of LED
+    // Take the semaphore before putting messages in the queue
     if (xSemaphoreTake(mutex, 0) == pdTRUE) {
-      for(int i = 0; i < uxQueueMessagesWaiting( msg_queue); i++){
-          if (xQueueReceive(msg_queue, (void *)&pitchToIndex, 10) == pdTRUE) {
+      for(int i = 0; i < uxQueueMessagesWaiting( midi_queue ); i++){
+          if (xQueueReceive(midi_queue , (void *)&pitchToIndex, 10) == pdTRUE) {
             //Set led to red if an on note is sent
             //Serial.println(pitchToIndex);
+            // Determine if the integer is on or off
             if((pitchToIndex & ON_BIT_MASK) == ON_BIT_MASK){
-              firstLedRow[(pitchToIndex & (~ON_BIT_MASK))] = 1;//remove "ON" bit component
+              firstLedRow[(pitchToIndex & (~ON_BIT_MASK))] = ON;//remove "ON" bit component
             }else{
-              firstLedRow[pitchToIndex] = 0;
+              firstLedRow[pitchToIndex] = OFF;
             }   
           }   
       }
@@ -110,11 +109,14 @@ void setFrame(void *parameters){
 
 void setup()
 {
-  for( int i = 0; i < NUM_LEDS; i++){
+  // Clear all the LEDs to black 
+  for( uint8_t i = 0; i < NUM_LEDS; i++){
     leds[i] = CRGB::Black;
   }
+  // Create a mutex for the message queue
   mutex = xSemaphoreCreateMutex();
-  msg_queue = xQueueCreate(MSG_QUEUE_LEN, sizeof(int));
+  // Create a queue of length of the number of rows
+  midi_queue  = xQueueCreate(MIDI_QUEUE_LEN, sizeof(uint8_t));
   Serial.begin(115200);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
   midi2.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
@@ -123,6 +125,7 @@ void setup()
     // is received. In this case it's "MyHandleNoteOn".
   midi2.setHandleNoteOff(MyHandleNoteOff); // This command tells the Midi Library 
   pinMode (LED, OUTPUT); // Set Arduino board pin 13 to output
+  //Pin show Frame task to CPU 0 
   xTaskCreatePinnedToCore(
                         showFrame,   /* Task function. */
                         "Show Frame",     /* name of task. */
@@ -131,6 +134,7 @@ void setup()
                         1,           /* priority of the task */
                         NULL,      /* Task handle to keep track of created task */
                         pro_cpu);          /* pin task to core 0 */
+  //Pin Set Frame task to CPU 1
   xTaskCreatePinnedToCore(
                         setFrame,   /* Task function. */
                         "Set Frame",     /* name of task. */
@@ -143,38 +147,49 @@ void setup()
 
 void loop()
 {
-    // Read incoming messages
+    // Read incoming MIDI messages
     midi2.read();
 }
 
-
+//ISR for when a note on event is detected
 void MyHandleNoteOn(byte channel, byte pitch, byte velocity) { 
-  //digitalWrite(LED,HIGH);  //Turn LED on
+  //digitalWrite(LED,HIGH);  //Turn debug LED on
  // Serial.println(pitch);
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-
-  int pitchToIndex = pitch - 36;
-  //add on bit so there is a difference between on and off messages
+  // Convert Pitch to index
+  uint8_t pitchToIndex = pitch - 36;
+  // Add on bit so there is a difference between on and off messages
   pitchToIndex = pitchToIndex | ON_BIT_MASK;
-  //Serial.println(pitchToIndex);
-  if (xQueueSendFromISR(msg_queue, (void *)&pitchToIndex, &xHigherPriorityTaskWoken) != pdTRUE) {
+  // Serial.println(pitchToIndex);
+  // Only enqueue value if queue isn't full
+  if (xQueueSendFromISR(midi_queue , (void *)&pitchToIndex, &xHigherPriorityTaskWoken) != pdTRUE) {
     Serial.println("Queue Full");
   }
  
 }
+//ISR for when a note off event is detected
 void MyHandleNoteOff(byte channel, byte pitch, byte velocity) { 
-  //digitalWrite(LED,LOW);  //Turn LED off
+  //digitalWrite(LED,LOW);  //Turn debug LED off
   //Serial.println(pitch);
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  byte pitchToIndex = pitch - 36;
-  if (xQueueSendFromISR(msg_queue, (void *)&pitchToIndex, &xHigherPriorityTaskWoken) != pdTRUE) {
+  uint8_t pitchToIndex = pitch - 36;
+  if (xQueueSendFromISR(midi_queue, (void *)&pitchToIndex, &xHigherPriorityTaskWoken) != pdTRUE) {
     Serial.println("Queue Full");
   }
 }
 
+/***********************************************************
+Functions to convert coordinates to an index on an LED matrix
+The LED matrix is structured in a snaking pattern as shown below
+0 | 15 | 16 |... 
+1 | 14 | 17 |...
+. | .  | .  |...
+7 | 8  | 23 |...
+************************************************************/
+// Converts a coordinate to the index on a LED matrix 
 int MatrixToLedString(coord coord, int length){
 
-  int index;
+  uint8_t index;
   index = length * coord.x;
   if((coord.x % 2) == 0){
     index += coord.y;
@@ -185,6 +200,7 @@ int MatrixToLedString(coord coord, int length){
 
 }
 
+// Converts a LED index on a strip to a coordinate
 void LedStripToMatrix(int index, coord *coord ){
   if((index/8)%2 == 0){
     coord->y = index%8;
